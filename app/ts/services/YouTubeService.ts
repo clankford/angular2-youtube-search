202afd638a88c66ce5e54f0210871e3e362b1257
Chris Lankford <christopherlankford@gmail.com>
2016-02-23 15:54:34 -0800

    Implemented search method to utilize the YouTube APIs

--- app/ts/services/YouTubeService.ts
+++ app/ts/services/YouTubeService.ts
@@ -1,12 +1,38 @@
 import { Injectable, Inject, bind } from 'angular2/core';
-import { Http } from 'angular2/http';
+import { Http, Response } from 'angular2/http';
 import { YOUTUBE_API_KEY, YOUTUBE_API_URL } from './services';
+import { Observable } from 'rxjs';
+import { SearchResult } from '../models/SearchResult';
 
 @Injectable()
 export class YouTubeService {
   constructor(public http: Http,
               @Inject(YOUTUBE_API_KEY) private apiKey: string,
               @Inject(YOUTUBE_API_URL) private apiUrl: string) {
+  }
+
+  search(query: string): Observable<SearchResult[]> {
+    // Construct the query string for the API call
+    let params: string = [
+      `q=${query}`,
+      `key=${this.apiKey}`,
+      `part=snippet`,
+      `type=video`,
+      `maxResults=10`
+    ].join('&');
+    let queryUrl: string = `${this.apiUrl}?${params}`;
+
+    return this.http.get(queryUrl)
+      .map((response: Response) => {
+        return(<any>response.json()).items.map(item => {
+          return new SearchResult({
+            id: item.id.videoId,
+            title: item.snippet.title,
+            description: item.snippet.description,
+            thumbnailUrl: item.snippet.thumbnails.high.url
+          });
+        });
+      });
   }
 }
 
